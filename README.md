c9401cf74e39ba21fde7e2513804658b55804ce2
jeremykenedy <jeremy@grayboxpdx.com>
2016-03-20 21:56:58 -0700

    working on gulpfile

--- README.md
+++ README.md
@@ -4,19 +4,19 @@ Save position and resize properties of jQuery UI to a SQL table.
 
 | jquery-ui-position-sql-save Features  |
 | :------------ |
-| Light front end |
+| Bootstrap 4.0 front end |
 | Uses jQuery and jQuery UI |
 | Uses MySQL database to save jQuery UI settings |
 | Uses BOWER to manage dependencies |
 | Uses Node Package Manager (NPM) to pull dev dependencies|
-| Uses GULP to setup setup assets file structure and compile LESS, SCSS, and JS |
-| Uses SASS and Bootstrap LESS libraries|
+| Uses GULP to setup setup assets file structure and compile SCSS, and JS |
 
 ### Quick Project Setup
 ###### (Not including your dev environment)
 1. Run `git clone https://github.com/jeremykenedy/jquery-ui-position-sql-save.git jquery-ui-position-sql-save.git`
 2. From the projects root run `bower update`
 3. From the projects root run `npm install`
+
 
 2. Create a MySQL database for the project
     * ```mysql -u {SQL_USERNAME} -p```
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 /*
- If this style of commenting is frowned upon please let me know. Thanks.
  |--------------------------------------------------------------------------
- | Elixir Through Gulp Asset Management
- |--------------------------------------------------------------------------
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | GULP JS provides a clean, fluent API for defining some basic Gulp tasks
  | for your application. By default, we are compiling the Sass and JS
  | file for our application, as well as publishing vendor resources.
- |--------------------------------------------------------------------------
- |  ######   #######  ##     ## ##     ##    ###    ##    ## ########   ######
- | ##    ## ##     ## ###   ### ###   ###   ## ##   ###   ## ##     ## ##    ##
- | ##       ##     ## #### #### #### ####  ##   ##  ####  ## ##     ## ##
- | ##       ##     ## ## ### ## ## ### ## ##     ## ## ## ## ##     ##  ######
- | ##       ##     ## ##     ## ##     ## ######### ##  #### ##     ##       ##
- | ##    ## ##     ## ##     ## ##     ## ##     ## ##   ### ##     ## ##    ##
- |  ######   #######  ##     ## ##     ## ##     ## ##    ## ########   ######
- |--------------------------------------------------------------------------
- | Terminal/Bash Commands
- |--------------------------------------------------------------------------
- | sudo gulp copyfile 		<= 	Runs Gulp to copy assets
- | sudo gulp 				<= 	Runs Gulp to compile assets
- |--------------------------------------------------------------------------
  |--------------------------------------------------------------------------
  | ########    ###    ########  ##       ########     #######  ########     ######   #######  ##    ## ######## ######## ##    ## ########  ######
  |    ##      ## ##   ##     ## ##       ##          ##     ## ##          ##    ## ##     ## ###   ##    ##    ##       ###   ##    ##    ##    ##
@@ -31,50 +14,64 @@
  |--------------------------------------------------------------------------
  | TABLE OF CONTENTS
  |--------------------------------------------------------------------------
- |	 |	 1. Loads - Core and Plugin Preprocessors load calls
- |	 |	 2. Controllers - Choose your options
- |	 |	 3. Assets - Get and move preprocessor assets
- |	 |	 4. APP CSS & JS - APP CSS & JS Preproccessors
- |	 |	 5. PHPUnit Testing - PHPUNIT Load Testing
+ |	 |	 1. Commands 		- Quick List of GULP Commands
+ |	 |	 2. Loads 			- Preprocessors load calls
+ |	 |	 3. Controllers 	- Choose what ot compile
+ |	 |	 4. Assets 			- File asset management
+ |	 |	 5. APP CSS & JS 	- Process Assets
  |--------------------------------------------------------------------------
  |--------------------------------------------------------------------------
- |   ##                 ##        #######     ###    ########   ######
- | ####                 ##       ##     ##   ## ##   ##     ## ##    ##
- |   ##                 ##       ##     ##  ##   ##  ##     ## ##
- |   ##      #######    ##       ##     ## ##     ## ##     ##  ######
- |   ##                 ##       ##     ## ######### ##     ##       ##
- |   ##                 ##       ##     ## ##     ## ##     ## ##    ##
- | ######               ########  #######  ##     ## ########   ######
+ |   ##                  ######   #######  ##     ## ##     ##    ###    ##    ## ########   ######
+ | ####                 ##    ## ##     ## ###   ### ###   ###   ## ##   ###   ## ##     ## ##    ##
+ |   ##                 ##       ##     ## #### #### #### ####  ##   ##  ####  ## ##     ## ##
+ |   ##      #######    ##       ##     ## ## ### ## ## ### ## ##     ## ## ## ## ##     ##  ######
+ |   ##                 ##       ##     ## ##     ## ##     ## ######### ##  #### ##     ##       ##
+ |   ##                 ##    ## ##     ## ##     ## ##     ## ##     ## ##   ### ##     ## ##    ##
+ | ######                ######   #######  ##     ## ##     ## ##     ## ##    ## ########   ######
+ |--------------------------------------------------------------------------
+ | Terminal/Bash Commands
+ |--------------------------------------------------------------------------
+ | sudo gulp copyfile 		<= 	Runs Gulp to copy assets
+ | sudo gulp 				<= 	Runs Gulp to compile assets
  |--------------------------------------------------------------------------
  |--------------------------------------------------------------------------
- | Loads - Core and Plugin Preprocessors load calls
+ |  #######                ##        #######     ###    ########   ######
+ | ##     ##               ##       ##     ##   ## ##   ##     ## ##    ##
+ |        ##               ##       ##     ##  ##   ##  ##     ## ##
+ |  #######     #######    ##       ##     ## ##     ## ##     ##  ######
+ | ##                      ##       ##     ## ######### ##     ##       ##
+ | ##                      ##       ##     ## ##     ## ##     ## ##    ##
+ | #########               ########  #######  ##     ## ########   ######
+ |--------------------------------------------------------------------------
+ |--------------------------------------------------------------------------
+ | Loads - Core GULP and Plugin Preprocessors loads
  |--------------------------------------------------------------------------
  */
 	var gulp = require('gulp'),
-		elixir = require('laravel-elixir'),
-	    sass = require('gulp-ruby-sass'),
-	    autoprefixer = require('gulp-autoprefixer'),
+	    del = require('del'),
+	    less = require('gulp-less'),
+	    cache = require('gulp-cache'),
 	    nano = require('gulp-cssnano'),
 	    jshint = require('gulp-jshint'),
 	    uglify = require('gulp-uglify'),
-	    imagemin = require('gulp-imagemin'),
 	    rename = require('gulp-rename'),
 	    concat = require('gulp-concat'),
 	    notify = require('gulp-notify'),
-	    cache = require('gulp-cache'),
+	    sass = require('gulp-ruby-sass'),
+	    imagemin = require('gulp-imagemin'),
 	    livereload = require('gulp-livereload'),
-	    del = require('del'),
-	    less = require('gulp-less'),
-	    lessToScss = require('gulp-less-to-scss');
+		minifyHTML = require('gulp-minify-html'),
+	    lessToScss = require('gulp-less-to-scss'),
+	    autoprefixer = require('gulp-autoprefixer');
 /*
  |--------------------------------------------------------------------------
  |  #######                 ######   #######  ##    ## ######## ########   #######  ##       ##       ######## ########
  | ##     ##               ##    ## ##     ## ###   ##    ##    ##     ## ##     ## ##       ##       ##       ##     ##
  |        ##               ##       ##     ## ####  ##    ##    ##     ## ##     ## ##       ##       ##       ##     ##
  |  #######     #######    ##       ##     ## ## ## ##    ##    ########  ##     ## ##       ##       ######   ########
- | ##                      ##       ##     ## ##  ####    ##    ##   ##   ##     ## ##       ##       ##       ##   ##
- | ##                      ##    ## ##     ## ##   ###    ##    ##    ##  ##     ## ##       ##       ##       ##    ##
- | #########                ######   #######  ##    ##    ##    ##     ##  #######  ######## ######## ######## ##     ##
+ |        ##               ##       ##     ## ##  ####    ##    ##   ##   ##     ## ##       ##       ##       ##   ##
+ | ##     ##               ##    ## ##     ## ##   ###    ##    ##    ##  ##     ## ##       ##       ##       ##    ##
+ |  #######                 ######   #######  ##    ##    ##    ##     ##  #######  ######## ######## ######## ##     ##
  |--------------------------------------------------------------------------
  |--------------------------------------------------------------------------
  | ASSET MANAGEMENT CONTROLLER - Choose which assets to compile when running gulp
@@ -89,18 +86,15 @@
 	var enable_compile_app_css		= 1;			// Compile App SCSS & CSS?
 	var enable_compile_app_js		= 1;			// Compile App JS?
 
-	/* PHPUNIT -----------------------------------------------------------------*/
-	var enable_unit_testing 		= 0;			// Compile phpUnit Testing?
-
 /*
  |--------------------------------------------------------------------------
- |  #######                   ###     ######   ######  ######## ########  ######
- | ##     ##                 ## ##   ##    ## ##    ## ##          ##    ##    ##
- |        ##                ##   ##  ##       ##       ##          ##    ##
- |  #######     #######    ##     ##  ######   ######  ######      ##     ######
- |        ##               #########       ##       ## ##          ##          ##
- | ##     ##               ##     ## ##    ## ##    ## ##          ##    ##    ##
- |  #######                ##     ##  ######   ######  ########    ##     ######
+ | ##                         ###     ######   ######  ######## ########  ######
+ | ##    ##                  ## ##   ##    ## ##    ## ##          ##    ##    ##
+ | ##    ##                 ##   ##  ##       ##       ##          ##    ##
+ | ##    ##     #######    ##     ##  ######   ######  ######      ##     ######
+ | #########               #########       ##       ## ##          ##          ##
+ |       ##                ##     ## ##    ## ##    ## ##          ##    ##    ##
+ |       ##                ##     ##  ######   ######  ########    ##     ######
  |--------------------------------------------------------------------------
  | Gulp Asset Management - Create Gulp function copyfiles
  |--------------------------------------------------------------------------
@@ -112,109 +106,61 @@ gulp.task("copyfiles", function() {
 
 	/*
 	 |--------------------------------------------------------------------------
-	 | Copy Bootstrap and FontAwesome
+	 | Copy Fonts
 	 |--------------------------------------------------------------------------
 	 */
-	gulp.src("vendor/bower_dl/bootstrap/**")
-		.pipe(gulp.dest("resources/assets/bootstrap/"));
-
-	gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
-		.pipe(gulp.dest("public/assets/fonts"));
-
-	gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
-		.pipe(gulp.dest("public/assets/css/fonts"));
-
-	gulp.src("vendor/bower_dl/fontawesome/**")
-		.pipe(gulp.dest("resources/assets/fontawesome/"));
 
 	gulp.src("vendor/bower_dl/fontawesome/fonts/**")
-		.pipe(gulp.dest("public/assets/css/fonts"));
-
-	gulp.src("vendor/bower_dl/ionicons/**")
-		.pipe(gulp.dest("resources/assets/ionicons/"));
-
-	gulp.src("vendor/bower_dl/ionicons/fonts/**")
-		.pipe(gulp.dest("public/assets/css/fonts"));
-
-	/*
-	 |--------------------------------------------------------------------------
-	 | Copy jQuery and jQuery UI JS Assets
-	 |--------------------------------------------------------------------------
-	 */
-	gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
-		.pipe(gulp.dest("resources/assets/js/"));
-
-	gulp.src("vendor/bower_dl/jquery-ui/jquery-ui.js")
-		.pipe(gulp.dest("resources/assets/js/"));
+		.pipe(gulp.dest("build/fonts/"));
 
 });
 
 /*
  |--------------------------------------------------------------------------
- | Default gulp is to run this elixir laravel function - builds on gulp
+ | ########                  ###    ########  ########
+ | ##                       ## ##   ##     ## ##     ##
+ | ##                      ##   ##  ##     ## ##     ##
+ | #######     #######    ##     ## ########  ########
+ |       ##               ######### ##        ##
+ | ##    ##               ##     ## ##        ##
+ |  ######                ##     ## ##        ##
  |--------------------------------------------------------------------------
  */
-elixir(function(mix) {
 
-	/*
-	 |--------------------------------------------------------------------------
-	 | ##                         ###    ########  ########
-	 | ##    ##                  ## ##   ##     ## ##     ##
-	 | ##    ##                 ##   ##  ##     ## ##     ##
-	 | ##    ##     #######    ##     ## ########  ########
-	 | #########               ######### ##        ##
-	 |       ##                ##     ## ##        ##
-	 |       ##                ##     ## ##        ##
-	 |--------------------------------------------------------------------------
-	 */
-	// PROCESS APP ASSETS
-	if (enable_compile_app) {
+// elixir(function(mix) {
 
-		// COMPILE BOOTSTRAP LESS
-		mix.less('vender/bower_dl/bootstrap/less/bootstrap.less', '/css/vendor/bootstrap.css');
+// 	// PROCESS APP ASSETS
+// 	if (enable_compile_app) {
 
-		// COMIPILE APP SASS/SCSS - APP CSS
-		if (enable_compile_app_css) {
-			mix.sass('sass/app.scss', 'public/assets/css/app.css');
-		}
+// 		// COMPILE BOOTSTRAP LESS
+// 		mix.less('vender/bower_dl/bootstrap/less/bootstrap.less', '/css/vendor/bootstrap.css');
 
-		// COMBINE CSS INTO SINGLE FILE
-	    mix.styles([
+// 		// COMIPILE APP SASS/SCSS - APP CSS
+// 		if (enable_compile_app_css) {
+// 			mix.sass('sass/app.scss', 'public/assets/css/app.css');
+// 		}
 
-			'/css/vendor/bootstrap.css',					// COMPILED BOOTSTRAP FILE
-			'/css/app.css',
+// 		// COMBINE CSS INTO SINGLE FILE
+// 	    mix.styles([
 
-	    ],
-	    'public/assets/css/admin/style.css', './');													// SINGLE FILE OUTPUT
+// 			'/css/vendor/bootstrap.css',					// COMPILED BOOTSTRAP FILE
+// 			'/css/app.css',
 
-		//COMBINE APP SCRIPTS - APP JS
-		if (enable_compile_app_js) {
-		    mix.scripts([
-					'js/jquery.js',
-					'js/bootstrap.js',
-					'js/app.js',
-			    ],
-			    'public/assets/js/app.js',
-			    'resources/assets'
-		   	);
-		}
-	}
+// 	    ],
+// 	    'public/assets/css/admin/style.css', './');													// SINGLE FILE OUTPUT
 
-	/*
-	 |--------------------------------------------------------------------------
-     | ########               ######## ########  ######  ######## #### ##    ##  ######
-     | ##                        ##    ##       ##    ##    ##     ##  ###   ## ##    ##
-     | ##                        ##    ##       ##          ##     ##  ####  ## ##
-     | #######     #######       ##    ######    ######     ##     ##  ## ## ## ##   ####
-     |       ##                  ##    ##             ##    ##     ##  ##  #### ##    ##
-     | ##    ##                  ##    ##       ##    ##    ##     ##  ##   ### ##    ##
-     |  ######                   ##    ########  ######     ##    #### ##    ##  ######
-	 |--------------------------------------------------------------------------
-	 */
-	// START phpUnit FOR UNIT TESTING
-	if (enable_unit_testing) {
-		mix.phpUnit();
-	}
+// 		//COMBINE APP SCRIPTS - APP JS
+// 		if (enable_compile_app_js) {
+// 		    mix.scripts([
+// 					'js/jquery.js',
+// 					'js/bootstrap.js',
+// 					'js/app.js',
+// 			    ],
+// 			    'public/assets/js/app.js',
+// 			    'resources/assets'
+// 		   	);
+// 		}
+// 	}
 
-});
+// });
 
